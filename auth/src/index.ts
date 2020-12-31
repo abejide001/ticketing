43c6bee75ee55f9c2b959052f3b56011eb104ec8
abejide001 <abejidefemi1@gmail.com>
2021-01-01 00:59:20 +0100

    implement auth service

---
+++ auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+const port = 3000 || process.env.PORT
+
+app.listen(port, () => {
+    console.log(`app listening on ${port}`)
+})
