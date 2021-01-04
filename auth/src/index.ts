efe67a1399d3513ec8cd2cc7c7bd1f978f0e0f96
abejide001 <abejidefemi1@gmail.com>
2021-01-05 00:29:59 +0100

    setup gcloud for skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,9 @@ app.use(express.json())
 
 const port = 3000 || process.env.PORT
 
+app.get('/api/users/currentUser', (req, res) => {
+    res.send("hi there")
+})
 app.listen(port, () => {
     console.log(`app listeniing on ${port}`)
 })
